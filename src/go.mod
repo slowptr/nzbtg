93f3290f16aa0942837aa518233914a7a9b04afc
slowptr <42156104+slowptr@users.noreply.github.com>
2023-02-04 16:29:10 +0100

    Add simple telegram bot

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
 module github.com/slowptr/nzbtg
 
 go 1.18
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
--- src/main.go
+++ src/main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	token := os.Getenv("TG_TOKEN")
+	if token == "" {
+		log.Fatal("TG_TOKEN is not set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		document := update.Message.Document
+		if document != nil {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("file received: %s", document.FileName))
+			bot.Send(msg)
+		}
+	}
 }
