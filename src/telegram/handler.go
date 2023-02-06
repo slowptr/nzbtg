87c81cd6f9740ecad2ec5fdbfee862fd60fcb8e4
slowptr <yeetyoten3@gmail.com>
2023-02-06 01:44:31 +0100

    Seperate handler into more functions

--- src/telegram/handler.go
+++ src/telegram/handler.go
@@ -45,6 +45,91 @@ func checkPassword(url string) bool {
 	return strings.Contains(str, "<meta type=\"password\">")
 }
 
+func (t *Telegram) updateDownloadMessage(nzb *sabnzbd.SABNZBD, chatID int64, editMsgID int) string {
+	folderName := ""
+	for {
+		status, err := nzb.GetQueue()
+		if err != nil {
+			msg := tgbotapi.NewEditMessageText(chatID, editMsgID, "unable to get queue status")
+			t.bot.Send(msg)
+			continue
+		}
+
+		if len(status.Slots) == 0 {
+			msg := tgbotapi.NewEditMessageText(chatID, editMsgID, "100%, download finished...")
+			t.bot.Send(msg)
+			break
+		}
+
+		currentSlot := status.Slots[0]
+		folderName = currentSlot.FileName
+		msg := tgbotapi.NewEditMessageText(chatID, editMsgID, status.Slots[0].Percentage+"%")
+		t.bot.Send(msg)
+
+		time.Sleep(2 * time.Second)
+	}
+
+	return folderName
+}
+
+func findFolder(parent string, search string) string { // we dont check for directory now
+	for {
+		files, err := ioutil.ReadDir(parent)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			if f.Name() != search {
+				continue
+			}
+
+			return filepath.Join(parent, f.Name())
+		}
+	}
+}
+
+func zipFolder(src string, dst string) error { // add splitting into multiple files
+	archive, err := os.Create(dst)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer archive.Close()
+
+	zipWriter := zip.NewWriter(archive)
+	defer zipWriter.Close()
+
+	filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		header, _ := zip.FileInfoHeader(info)
+		header.Name, _ = filepath.Rel(src, path)
+		header.Method = zip.Deflate
+
+		writer, err := zipWriter.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(writer, file)
+		return err
+	})
+
+	return nil
+}
+
 func (t *Telegram) messageHandler(u tgbotapi.Update, nzb *sabnzbd.SABNZBD, cloud *tgcloud.TGCloud) {
 	if !checkFile(u) {
 		msg := tgbotapi.NewMessage(u.Message.Chat.ID, "only .nzb files are supported")
@@ -77,122 +162,38 @@ func (t *Telegram) messageHandler(u tgbotapi.Update, nzb *sabnzbd.SABNZBD, cloud
 	msg := tgbotapi.NewMessage(u.Message.Chat.ID, "downloading...")
 	editable, _ := t.bot.Send(msg)
 
-	folderName := ""
-	log.Println("folderName: " + folderName)
-	for {
-		status, err := nzb.GetQueue()
-		if err != nil {
-			msg := tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, "unable to get queue status")
-			t.bot.Send(msg)
-			continue
-		}
-
-		if len(status.Slots) == 0 {
-			msg := tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, "100%, download finished...")
-			t.bot.Send(msg)
-			if folderName == "" {
-				continue
-			}
-			break
-		}
-
-		currentSlot := status.Slots[0]
-		folderName = currentSlot.FileName
-		msg := tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, status.Slots[0].Percentage+"%")
-		t.bot.Send(msg)
-
-		time.Sleep(2 * time.Second)
+	folderName := t.updateDownloadMessage(nzb, u.Message.Chat.ID, editable.MessageID)
+	if folderName == "" {
+		folderName = strings.Split(fileURL, "/")[len(strings.Split(fileURL, "/"))-1]
+		folderName = strings.Split(folderName, ".")[0]
 	}
 
 	log.Println("looking for folder: " + folderName)
+	src := findFolder(nzb.DLPath, folderName)
+	dst := src + ".zip"
 
-	found := false
-	for {
-		if found {
-			break
-		}
+	t.bot.Send(tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, "zipping... "+dst))
 
-		files, err := ioutil.ReadDir(nzb.DLPath)
-		if err != nil {
-			log.Fatal(err)
-		}
+	zipFolder(src, dst)
 
-		for _, f := range files {
-			log.Printf("found: %s, searching for: %s", f.Name(), folderName)
-			if f.Name() != folderName {
-				continue
-			}
+	t.bot.Send(tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, "uploading... "+dst))
 
-			found = true
+	err = cloud.Upload(dst, u.Message.Document.FileName)
+	if err != nil {
+		msg := tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, err.Error())
+		t.bot.Send(msg)
+		return
+	}
 
-			msg := tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, "found folder: "+folderName)
-			t.bot.Send(msg)
+	t.bot.Send(tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, "upload finished"))
 
-			src := filepath.Join(nzb.DLPath, f.Name())
-			dst := src + ".zip"
-			if f.IsDir() {
-				archive, err := os.Create(dst)
-				if err != nil {
-					log.Fatal(err)
-				}
-				defer archive.Close()
+	err = os.RemoveAll(src)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-				zipWriter := zip.NewWriter(archive)
-				defer zipWriter.Close()
-
-				filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
-					if info.IsDir() {
-						return nil
-					}
-
-					if err != nil {
-						return err
-					}
-
-					file, err := os.Open(path)
-					if err != nil {
-						return err
-					}
-					defer file.Close()
-
-					header, _ := zip.FileInfoHeader(info)
-					header.Name, _ = filepath.Rel(src, path)
-					header.Method = zip.Deflate
-
-					writer, err := zipWriter.CreateHeader(header)
-					if err != nil {
-						return err
-					}
-
-					_, err = io.Copy(writer, file)
-					return err
-				})
-
-				msg := tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, "zipping and uploading... "+dst)
-				t.bot.Send(msg)
-
-				err = cloud.Upload(dst, u.Message.Document.FileName)
-				if err != nil {
-					msg := tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, err.Error())
-					t.bot.Send(msg)
-					return
-				}
-
-				msg = tgbotapi.NewEditMessageText(u.Message.Chat.ID, editable.MessageID, "upload finished")
-				t.bot.Send(msg)
-
-				err = os.RemoveAll(src)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				err = os.Remove(dst) // is still occupied, don't wanna sleep / loop tho
-				if err != nil {
-					log.Println(err)
-				}
-			}
-		}
-
-		time.Sleep(5 * time.Second)
+	err = os.Remove(dst) // is still occupied, don't wanna sleep / loop tho
+	if err != nil {
+		log.Println(err)
 	}
 }
